b89af14bec8b493e36e36bd71b632668ce30014f
ahmedooo1 <aahmadooo997@gmail.com>
2023-02-16 17:07:37 +0100

    authUpdate

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -1,7 +1,7 @@
 import 'package:balancetonpoids/main.dart';
 import 'package:balancetonpoids/utils/widgets.dart';
 import 'package:flutter/material.dart';
-
+//signup
 class SignupPage extends StatelessWidget {
   const SignupPage({super.key});
 
@@ -67,7 +67,7 @@ class SignupPage extends StatelessWidget {
   }
 }
 
-
+//login
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
   @override
@@ -114,7 +114,7 @@ class LoginPage extends StatelessWidget {
                         ),
                         Padding(
                           padding: const EdgeInsets.only(top: 20),
-                          child: button(context, "Se connecter", Colors.blue, Colors.white, Colors.white, 0, const MainPage()),
+                          child: button(context, "Se connecter", Colors.blue, Colors.white, Colors.white, 0,  ResetPasswordPage()),
                         ),
                         Padding(
                           padding: const EdgeInsets.only(top: 20),
@@ -223,6 +223,124 @@ class _MdpOubliePageState extends State<MdpOubliePage> {
             ),
           ),
         ),
+      ),
+    );
+  }
+}
+
+//réinitialiser le mdp
+class ResetPasswordPage extends StatelessWidget {
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController = TextEditingController();
+
+  bool _isPasswordValid() {
+    String password = _passwordController.text.trim();
+    return password.isNotEmpty && password.length >= 8;
+  }
+
+  bool _doPasswordsMatch() {
+    String password = _passwordController.text.trim();
+    String confirmPassword = _confirmPasswordController.text.trim();
+    return password == confirmPassword;
+  }
+
+  void _resetPassword(BuildContext context) {
+    if (_isPasswordValid() && _doPasswordsMatch() ) {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+
+            title: Text("Félicitation! votre mot de passe réinitialisé", style: TextStyle(
+              color: Colors.green,
+            ),),
+            content: Text("Vous allez être rédiger vers la page de connexion"),
+            actions: <Widget>[
+              TextButton(
+                child: Text("OK"),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => LoginPage()),
+                  );
+                },
+              ),
+            ],
+          );
+        },
+      );
+
+    } else {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("Erreur de réinitialisation du mot de passe", style: TextStyle(
+              color: Colors.red,
+            ),),
+            content: Text("Veuillez entrer un mot de passe valide et assurez-vous que les deux champs de mot de passe correspondent."),
+            actions: <Widget>[
+              TextButton(
+                child: Text("OK"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBar("Réinitialiser le mot de passe", true, context),
+      body: SingleChildScrollView(
+        child:Center( child: Padding(
+          padding: const EdgeInsets.all(30),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 100,
+                height: 100,
+                child: Image.asset(
+                  'assets/R-mdp.png',
+                  fit: BoxFit.fill,
+                ),
+              ),
+              const SizedBox(height: 40),
+              inputForm(_passwordController, "Nouveau mot de passe", const Icon(
+                Icons.lock,
+                color: Colors.grey,
+              ), ),
+              inputForm(_confirmPasswordController, "Confirmez le nouveau mot de passe", const Icon(
+                Icons.lock,
+                color: Colors.grey,
+              ), ),
+              Padding(
+                padding: const EdgeInsets.only(top: 16.0),
+                child: ElevatedButton(
+                  onPressed:  () {
+                    _resetPassword(context);
+                  },
+                  child: Text("Réinitialiser le mot de passe"),
+                ),
+              ),
+              const SizedBox(height: 250,),
+              const Text(
+                "Contactez nous par mail pour tout renseignement : info@btp.fr",
+                style: TextStyle(fontSize: 16),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 20),
+            ],
+          ),
+        ),)
+
       ),
     );
   }
