b6cb7b9aae5da8e7f76f457e064dae4bc07b63fb
ahmedooo1 <aahmadooo997@gmail.com>
2023-02-24 19:41:20 +0100

    page modifier profil =btn donnees perso

--- lib/pages/modifier_profil.dart
+++ lib/pages/modifier_profil.dart
@@ -25,7 +25,27 @@ class EditProfilePage extends StatelessWidget {
             SizedBox(height: 10.0),
             _buildPersonalInfoForm(),
             SizedBox(height: 40.0),
-            _buildCredentialsForm(),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => EditProfileDetailsPage()),
+            );
+          },
+          style: ButtonStyle(
+            padding: MaterialStateProperty.all(EdgeInsets.all(20)),backgroundColor: MaterialStatePropertyAll(Colors.green),
+            textStyle: MaterialStateProperty.all(TextStyle(fontSize: 20)),
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12.0),
+              ),
+            ),
+          ),
+          child: const Text(
+            'Modifier mes Données Personnelles',),
+
+
+        ),
             SizedBox(height: 20.0),
             ElevatedButton(
               onPressed: () {
@@ -139,66 +159,95 @@ class EditProfilePage extends StatelessWidget {
       ),
     );
   }
+}
 
-  Widget _buildCredentialsForm() {
-    return Container(
-      padding: EdgeInsets.all(16.0),
-      decoration: BoxDecoration(
-        color: Colors.blue,
-        borderRadius: BorderRadius.circular(12.0),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Text(
-            'Données personnelles',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 18.0,
-              color: Colors.white,
+
+class EditProfileDetailsPage extends StatelessWidget {
+  const EditProfileDetailsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBar('Données Personnelles', true, context),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(20.0),
+        child: Form(
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Image.asset(
+                  'assets/donnesPerso.png',height: 200,width: 150,
+                ),
+
+
+                Text(
+                  'Données personnelles',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18.0,
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(height: 16.0),
+                TextFormField(
+                  style: TextStyle(color: Colors.black),
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    fillColor: Colors.white,
+                    filled: true,
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                      borderSide: BorderSide(color: Colors.blue,width: 3),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                      borderSide: BorderSide(color: Colors.blue,width: 3),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 12),
+                TextFormField(
+                  style: TextStyle(color: Colors.black),
+                  decoration: InputDecoration(
+                    labelText: 'Mot de passe',
+                    fillColor: Colors.white,
+                    filled: true,
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                      borderSide: BorderSide(color: Colors.blue,width: 3),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                      borderSide: BorderSide(color: Colors.blue, width: 3),
+                    ),
+                  ),
+                  obscureText: true,
+                ),
+                SizedBox(height: 20.0),
+
+                ElevatedButton(
+              onPressed: () {
+                // Enregistrer les modifications
+              },
+              child: Text('Enregistrer'),
+              style: ButtonStyle(
+                padding: MaterialStateProperty.all(EdgeInsets.all(20)),
+                textStyle: MaterialStateProperty.all(TextStyle(fontSize: 20)),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12.0),
+                  ),
+                ),
+              ),
+            ),
+              ],
             ),
           ),
-          SizedBox(height: 16.0),
-          TextFormField(
-            style: TextStyle(color: Colors.black),
-            decoration: InputDecoration(
-              labelText: 'Email',
-              fillColor: Colors.white,
-              filled: true,
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(8.0),
-                borderSide: BorderSide(color: Colors.white),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(8.0),
-                borderSide: BorderSide(color: Colors.white),
-              ),
-            ),
-          ),
-          SizedBox(height: 8.0),
-          TextFormField(
-            style: TextStyle(color: Colors.black),
-            decoration: InputDecoration(
-              labelText: 'Mot de passe',
-              fillColor: Colors.white,
-              filled: true,
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(8.0),
-                borderSide: BorderSide(color: Colors.white),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(8.0),
-                borderSide: BorderSide(color: Colors.white),
-              ),
-            ),
-            obscureText: true,
-          ),
-        ],
+
+        ),
       ),
     );
   }
-
-
-
 }
