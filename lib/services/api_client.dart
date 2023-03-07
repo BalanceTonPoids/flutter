1247d1c5903c3cb5afb90da5da23f70dbe2a2dc2
Johann six <six.johann@gmail.com>
2023-03-07 16:44:16 +0100

    🎨 change token is now in secure storage

--- lib/services/api_client.dart
+++ lib/services/api_client.dart
@@ -1,18 +1,18 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:balancetonpoids/models/scale_data.dart';
 import 'package:balancetonpoids/models/user.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class ApiClient {
   final http.Client httpClient;
-  // final SharedPreferences sharedPreferences;
   final String apiUrl = 'http://localhost:3000/v1';
+  final storage = const FlutterSecureStorage();
 
-  ApiClient({required this.httpClient
-      // , required this.sharedPreferences
-      });
+  ApiClient({required this.httpClient});
 
   Future<Map<String, dynamic>> registerUser(
       String email, String password, String confirmPassword) async {
@@ -42,8 +42,9 @@ class ApiClient {
         case 200:
           // get token and set it in local storage
           var token = jsonDecode(response.body)['token'];
-          await SharedPreferences.getInstance()
-              .then((value) => value.setString('token', token));
+          await storage.write(key: 'token', value: token);
+          // await SharedPreferences.getInstance()
+          //     .then((value) => value.setString('token', token));
           // sharedPreferences.setString('token', token);
           return {'success': 'register success'};
         case 400:
@@ -68,8 +69,9 @@ class ApiClient {
         case 200:
           // get token and set it in local storage
           var token = jsonDecode(response.body)['token'];
-          await SharedPreferences.getInstance()
-              .then((value) => value.setString('token', token));
+          await storage.write(key: 'token', value: token);
+          // await SharedPreferences.getInstance()
+          //     .then((value) => value.setString('token', token));
           // sharedPreferences.setString('token', token);
           return true;
         case 400:
@@ -94,7 +96,33 @@ class ApiClient {
     );
     if (response.statusCode == 200) {
       // parse the user info from the response body and return it
-      return User.fromJson(response.body as Map<String, dynamic>);
+      final user =
+          User.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
+      // set the user in local storage
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.setString('name', user.name);
+      await prefs.setString('email', user.email);
+      await prefs.setString('phone', user.phone);
+      await prefs.setString('gender', user.gender);
+      await prefs.setString('metric', user.metric);
+      await prefs.setInt('age', user.age);
+      await prefs.setInt('height', user.height);
+      final lastScale = user.getLastScale();
+      if (lastScale != null) {
+        await prefs.setDouble('weight', lastScale.weight);
+      } else {
+        await prefs.setDouble('weight', 0.0);
+      }
+      final scaleDataList = user.scaleData;
+      if (scaleDataList.isNotEmpty) {
+        final List<String> encodedScaleDataList =
+            scaleDataList.map((e) => (e.toString())).toList();
+        await prefs.setStringList('scaleData', encodedScaleDataList);
+      } else {
+        await prefs.setStringList('scaleData', []);
+      }
+
+      return user;
     } else {
       throw Exception('Failed to get user info');
     }
@@ -102,9 +130,10 @@ class ApiClient {
 
   Future<String> getHeaderToken() async {
     // Retrieve the token from local storage
-    final token = await SharedPreferences.getInstance()
-        .then((value) => value.getString('token'));
+    // final token = await SharedPreferences.getInstance()
+    //     .then((value) => value.getString('token'));
     // final token = sharedPreferences.getString('token');
+    String? token = await storage.read(key: 'token');
     if (token != null) {
       return token;
     } else {
