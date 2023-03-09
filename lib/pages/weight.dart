c6b80c36a987201f78fcb44028ad6b37478bad21
Johann six <six.johann@gmail.com>
2023-03-09 16:37:47 +0100

    ✨ add auto load weight

--- lib/pages/weight.dart
+++ lib/pages/weight.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import '../utils/widgets.dart';
 
 class Weight extends StatefulWidget {
@@ -9,23 +10,33 @@ class Weight extends StatefulWidget {
 }
 
 class _WeightState extends State<Weight> {
+  final Future<SharedPreferences> prefs = SharedPreferences.getInstance();
+  late Future<double?> weight;
+
+  @override
+  void initState() {
+    super.initState();
+    weight = prefs.then((value) => value.getDouble('weight'));
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: appBar("Mesure du poids", false, context),
-      body: Column(
-        children: [
-          Container(
-            alignment: Alignment.topCenter,
-            child: Padding(
-              padding: const EdgeInsets.only(top: 20, bottom: 20),
-              child: weightStack(55.0, 'kg', Colors.blue, 200, 200),
-            ),
-          ),
-          loadingSpinner()
-        ]
-      )
-    );
+    return FutureBuilder<double?>(
+        future: weight,
+        builder: (context, snapshot) {
+          double showWeight = snapshot.data != null ? snapshot.data! : 0.0;
+          return Scaffold(
+              appBar: appBar('Mesure du poids', false, context),
+              body: Column(children: [
+                Container(
+                  alignment: Alignment.topCenter,
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 20, bottom: 20),
+                    child: weightStack(showWeight, 'kg', Colors.blue, 200, 200),
+                  ),
+                ),
+                loadingSpinner()
+              ]));
+        });
   }
 }
