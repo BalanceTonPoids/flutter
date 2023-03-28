b920562d1beddfe35209c2c99949775faea84de9
Alexis BRIET <alexis.briet2003@gmail.com>
2023-03-28 11:16:45 +0200

    Fix conflits

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -7,20 +7,14 @@
 #include "generated_plugin_registrant.h"
 
 #include <flutter_secure_storage_windows/flutter_secure_storage_windows_plugin.h>
-<<<<<<< HEAD
-#include <url_launcher_windows/url_launcher_windows.h>
-=======
 #include <permission_handler_windows/permission_handler_windows_plugin.h>
->>>>>>> 6a15856 (✨ choose of device)
+#include <url_launcher_windows/url_launcher_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   FlutterSecureStorageWindowsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FlutterSecureStorageWindowsPlugin"));
-<<<<<<< HEAD
-  UrlLauncherWindowsRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("UrlLauncherWindows"));
-=======
   PermissionHandlerWindowsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("PermissionHandlerWindowsPlugin"));
->>>>>>> 6a15856 (✨ choose of device)
+  UrlLauncherWindowsRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("UrlLauncherWindows"));
 }
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -4,11 +4,8 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   flutter_secure_storage_windows
-<<<<<<< HEAD
-  url_launcher_windows
-=======
   permission_handler_windows
->>>>>>> 6a15856 (✨ choose of device)
+  url_launcher_windows
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
